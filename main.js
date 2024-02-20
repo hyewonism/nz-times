667b35a1e48330e100382fefc038518e60db15e2
Hyewon Kang <hye1ey.k@gmail.com>
2024-02-20 19:30:28 +0900

    Updated url

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ let news = [];
 const getLatestNews = async () => {
   const url = new URL(
     // `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`
-    `https://curious-piroshki-943f87.netlify.app/top-headlines?q=${keyword}&country=kr&pageSize=${PAGE_SIZE}`
+    `https://curious-piroshki-943f87.netlify.app/top-headlines?q=${keyword}&country=kr&pageSize=${PAGE_SIZE}$apiKey=${API_KEY}`
   );
   const response = await fetch(url);
   const data = await response.json();
